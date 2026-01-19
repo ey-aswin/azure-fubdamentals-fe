a2e100dad10f7d1bc8a1444bca84aa5128a27b7f
Aswin . <aswin@gds.ey.com>
2026-01-19 23:16:50 +0530

    ENV mods

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:8000/api/v1";  
-export const SERVER_URL_V2 =  import.meta.env.SERVER_URL_V2 || "http://localhost:8000/api/v2"
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL    
+export const SERVER_URL_V2 =  import.meta.env.VITE_SERVER_URL_V2
